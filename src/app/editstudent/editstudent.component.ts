696dc1e40a8de56ac848effb722c859088e552b2
sulesuleman <mousuleman@gmail.com>
2020-05-30 22:02:26 +0500

    irst commit

---
+++ src/app/editstudent/editstudent.component.ts
@@ -0,0 +1,55 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
+@Component({
+  selector: 'app-editstudent',
+  templateUrl: './editstudent.component.html',
+  styleUrls: ['./editstudent.component.css']
+})
+export class EditstudentsComponent implements OnInit {
+  public student;
+  name:string;
+  sname:string;
+  srollno:string;
+    
+  constructor(private _data:DataService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+      this.getstudents();
+    
+  }
+
+getstudents(){
+  this.activatedRoute.params.subscribe(paramsId => {
+    this.name = paramsId.id;
+});
+   this._data.viewstudentbyid(this.name).subscribe(
+          data => { this.student = data},
+          err => console.error(err),
+          () => console.log("students retrieved successfully",this.student)
+      );
+    
+}
+
+editval(nme,roll)
+{
+  event.preventDefault();
+  // this.sname=stdname
+  // this.srollno=num
+let ret;
+// let body={
+//   id:this.name,
+//   name:this.sname,
+//   rollno:this.srollno
+// };
+this._data.updateStudent(this.name,nme,roll).subscribe(
+data => { ret = data},
+err => console.error(err),
+() => console.log('done loading Employees')
+  );
+alert("Student Updated Successfully");
+   this.getstudents();
+}
+
+}
+
